68a301443cfa8dfb2086389982b8624ffafc9abe
Pedro Henrique Molon Rodrigues <pedroh.molonr@gmail.com>
2026-08-05 17:00:07 -0300

    first commit

---
+++ src/main/java/com/pedromolon/CasaAndrade/CasaAndradeApplication.java
@@ -0,0 +1,13 @@
+package com.pedromolon.CasaAndrade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaAndradeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasaAndradeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedromolon/CasaAndrade/CasaAndradeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedromolon.CasaAndrade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaAndradeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
