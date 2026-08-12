827c9499bc3106c7bff805135c69588ae38b89a1
PedroMolon <pedroh.molonr@gmail.com>
2026-08-12 11:20:07 -0300

    feat: adiciona migration para user e role

---
+++ src/main/resources/db/migration/V3__create_user_role_table.sql
@@ -0,0 +1,22 @@
+CREATE TABLE tb_role (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE tb_user(
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE tb_user_role (
+    user_id BIGINT NOT NULL,
+    role_id BIGINT NOT NULL,
+    PRIMARY KEY (user_id, role_id),
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES tb_user(id) ON DELETE CASCADE,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES tb_role(id) ON DELETE CASCADE
+);
+
+INSERT INTO tb_role (name) VALUES ("ROLE_ADMIN") ON CONFLICT DO NOTHING;
+INSERT INTO tb_role (name) VALUES ("ROLE_SELLER") ON CONFLICT DO NOTHING;
