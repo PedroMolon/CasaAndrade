fd4a9dc23e8e746ec2ff4c25229b20e2e81d7171
PedroMolon <pedroh.molonr@gmail.com>
2026-08-12 14:16:17 -0300

    refactor: refatora o repository

--- src/main/java/com/pedromolon/CasaAndrade/repository/RoleRepository.java
+++ src/main/java/com/pedromolon/CasaAndrade/repository/RoleRepository.java
@@ -4,8 +4,6 @@ import com.pedromolon.CasaAndrade.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 }
