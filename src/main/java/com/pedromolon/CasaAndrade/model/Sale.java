8420e5d197d999bb8e7ef25b2c253583e1d65230
PedroMolon <pedroh.molonr@gmail.com>
2026-08-13 14:45:13 -0300

    feat: adiciona a entidade Sale

---
+++ src/main/java/com/pedromolon/CasaAndrade/model/Sale.java
@@ -0,0 +1,47 @@
+package com.pedromolon.CasaAndrade.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_sale")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Sale {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "client_id", nullable = false)
+    private Client client;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "seller_id", nullable = false)
+    private User user;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal total;
+
+    @Column(name = "sale_date", nullable = false)
+    private LocalDateTime saleDate;
+
+    @OneToMany(mappedBy = "sale")
+    private List<SaleItem> items = new ArrayList<>();
+
+    @PrePersist
+    void prePersist() {
+        this.saleDate = LocalDateTime.now();
+    }
+}
