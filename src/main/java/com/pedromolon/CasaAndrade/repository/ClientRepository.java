33a987bff102378923a445d2d9104bab54d79486
PedroMolon <pedroh.molonr@gmail.com>
2026-08-13 16:01:51 -0300

    feat: adiciona o repository para Client

---
+++ src/main/java/com/pedromolon/CasaAndrade/repository/ClientRepository.java
@@ -0,0 +1,13 @@
+package com.pedromolon.CasaAndrade.repository;
+
+import com.pedromolon.CasaAndrade.model.Client;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, Long> {
+    boolean existsByDocument(String document);
+    Page<Client> findByActive(Pageable pageable);
+}
